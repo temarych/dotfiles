57d538ac6d1b8ed3755628de28bbc8245287646f
temarych <teemest06@icloud.com>
2026-04-20 20:40:55 +0300

    feat(conform): setup autoformat with builtin option

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -5,16 +5,13 @@ return {
     local conform = require("conform")
 
     conform.setup({
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_format = "fallback",
+      },
       formatters_by_ft = {
         lua = { "stylua" },
       },
-    })
-
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      pattern = "*",
-      callback = function(args)
-        conform.format({ bufnr = args.buf })
-      end,
     })
   end,
 }
