ef25b28cd4fa018d711bf93a35c3c3f6c89be851
temarych <teemest06@icloud.com>
2026-04-20 22:59:28 +0300

    feat(statusline): improve nvim statusline ui

---
+++ nvim/.config/nvim/lua/plugins/mini.git.lua
@@ -0,0 +1,19 @@
+---@param args vim.api.keyset.create_autocmd.callback_args
+local function format_summary(args)
+  local summary = vim.b[args.buf].minigit_summary
+  vim.b[args.buf].minigit_summary_string = summary.head_name or ""
+end
+
+local function setup_summary_format()
+  local opts = { pattern = "MiniGitUpdated", callback = format_summary }
+  vim.api.nvim_create_autocmd("User", opts)
+end
+
+---@type PluginConfig
+return {
+  source = "https://github.com/nvim-mini/mini-git",
+  setup = function()
+    require("mini.git").setup()
+    setup_summary_format()
+  end,
+}
--- nvim/.config/nvim/lua/plugins/mini.statusline.lua
+++ nvim/.config/nvim/lua/plugins/mini.statusline.lua
@@ -1,6 +1,10 @@
 ---@type PluginConfig
 return {
   source = "https://github.com/nvim-mini/mini.statusline",
+  dependencies = {
+    "https://github.com/nvim-mini/mini-git",
+    "https://github.com/nvim-mini/mini.diff",
+  },
   setup = function()
     local MiniStatusline = require("mini.statusline")
 
@@ -8,18 +12,19 @@ return {
       content = {
         active = function()
           local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = math.huge })
-          local git = MiniStatusline.section_git({ trunc_width = 40 })
+          local git = MiniStatusline.section_git({ trunc_width = 75 })
           local diff = MiniStatusline.section_diff({ trunc_width = 75 })
           local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
           local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
 
           return MiniStatusline.combine_groups({
             { hl = mode_hl, strings = { mode:upper() } },
-            { hl = "MiniStatuslineDevinfo", strings = { git, diff } },
-            { hl = "MiniStatuslineDiagnostics", strings = { diagnostics } },
+            { hl = "Identifier", strings = { git } },
+            { hl = "WarningMsg", strings = { diff } },
+            { hl = "Normal", strings = { diagnostics } },
             "%<",
             "%=",
-            { hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
+            { hl = "Directory", strings = { fileinfo } },
           })
         end,
       },
