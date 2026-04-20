0e544129cd18ae9ec3e9155113cc6eea7d203314
temarych <teemest06@icloud.com>
2026-04-20 20:40:55 +0300

    feat(flash): add flash

---
+++ nvim/.config/nvim/lua/plugins/flash.lua
@@ -0,0 +1,12 @@
+---@type PluginConfig
+return {
+  source = "https://github.com/folke/flash.nvim",
+  setup = function()
+    local flash = require("flash")
+    vim.keymap.set({ "n", "x", "o" }, "s", flash.jump, { desc = "Flash" })
+    vim.keymap.set({ "n", "x", "o" }, "S", flash.treesitter, { desc = "Flash Treesitter" })
+    vim.keymap.set("o", "r", flash.remote, { desc = "Remote Flash" })
+    vim.keymap.set({ "o", "x" }, "R", flash.treesitter_search, { desc = "Treesitter Search" })
+    vim.keymap.set({ "c" }, "<c-s>", flash.toggle, { desc = "Toggle Flash Search" })
+  end
+}
