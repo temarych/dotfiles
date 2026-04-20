8e3ac47cecac206d026425c299272e6eaf282dac
temarych <teemest06@icloud.com>
2026-04-20 20:40:55 +0300

    feat(diff): replace gitsigns with mini.diff

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++
@@ -1,8 +0,0 @@
----@type PluginConfig
-return {
-  source = "https://github.com/lewis6991/gitsigns.nvim",
-  setup = function()
-    local gs = require("gitsigns")
-    vim.keymap.set("n", "<leader>hr", gs.reset_hunk, { desc = "Reset Hunk" })
-  end,
-}
---
+++ nvim/.config/nvim/lua/plugins/mini.diff.lua
@@ -0,0 +1,27 @@
+---@type PluginConfig
+return {
+  source = "https://github.com/nvim-mini/mini.diff",
+  setup = function()
+    local MiniDiff = require("mini.diff")
+
+    MiniDiff.setup({
+      view = {
+        style = "sign",
+        signs = { add = "▎", change = "▎", delete = "▎" },
+        priority = 199,
+      },
+      delay = {
+        text_change = 10,
+      },
+      mappings = {
+        apply = "gh",
+        reset = "gH",
+        textobject = "gh",
+        goto_first = "[H",
+        goto_prev = "[h",
+        goto_next = "]h",
+        goto_last = "]H",
+      },
+    })
+  end,
+}
