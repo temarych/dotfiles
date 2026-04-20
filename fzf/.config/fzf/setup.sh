6db0308dc57b46f376a7cb0a0779e4db7c3f0dc9
temarych <teemest06@icloud.com>
2026-04-20 20:40:54 +0300

    feat(starship): add git status & other modules)

--- fzf/.config/fzf/setup.sh
+++ fzf/.config/fzf/setup.sh
@@ -2,13 +2,13 @@
 
 export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
   --layout=reverse \
-  --color=bg:#1a1b26 \
+  --color=bg:-1 \
   --color=bg+:#414868 \
   --color=fg:#a9b1d6 \
   --color=fg+:#c0caf5 \
   --color=hl:#7dcfff \
   --color=hl+:#7dcfff \
-  --color=gutter:#1a1b26 \
+  --color=gutter:-1 \
   --color=info:#414868 \
   --color=marker:#bb9af7 \
   --color=pointer:#7dcfff \
