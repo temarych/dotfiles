2bb0887cad5159c41b3115cdb8b1e6e65688a32a
temarych <teemest06@icloud.com>
2026-04-20 20:40:54 +0300

    feat(lsp): extract lsp configs into lua/ dir for automatization

---
+++ nvim/.config/nvim/lsp/lua_ls.lua
@@ -0,0 +1,11 @@
+return {
+  cmd = { "lua-language-server" },
+  filetypes = { "lua" },
+  root_markers = { ".luarc.json", ".git" },
+  settings = {
+    Lua = {
+      runtime = { version = "LuaJIT" },
+      diagnostics = { globals = { "vim" } },
+    },
+  },
+}
--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -1,13 +1,1 @@
-vim.lsp.config('lua_ls', {
-  cmd = { "lua-language-server" },
-  filetypes = { "lua" },
-  root_markers = { ".luarc.json", ".git" },
-  settings = {
-    Lua = {
-      runtime = { version = "LuaJIT" },
-      diagnostics = { globals = { "vim" } },
-    },
-  },
-})
-
-vim.lsp.enable('lua_ls')
+vim.lsp.enable("lua_ls")
