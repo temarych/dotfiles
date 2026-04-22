e03c4d753807749387ca4d31ca19dd3732266ce1
temarych <teemest06@icloud.com>
2026-04-22 22:58:28 +0300

    feat(remap): add restart mapping

--- nvim/.config/nvim/lua/config/remap.lua
+++ nvim/.config/nvim/lua/config/remap.lua
@@ -3,3 +3,9 @@ vim.g.maplocalleader = " "
 
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic" })
 vim.keymap.set("n", "<leader>?", vim.cmd.map, { desc = "Show keymaps" })
+
+vim.keymap.set("n", "<leader>R", function()
+  local session = vim.fn.stdpath("state") .. "/restart_session.vim"
+  vim.cmd("mksession! " .. vim.fn.fnameescape(session))
+  vim.cmd("restart source " .. vim.fn.fnameescape(session))
+end, { desc = "Restart Neovim" })
