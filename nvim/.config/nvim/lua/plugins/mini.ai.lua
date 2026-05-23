8f9d6f18ae10d254ed9ba0f82d6e82a04e65dd38
temarych <teemest06@icloud.com>
2026-05-23 15:14:30 +0300

    feat(ai): add textobject for statements

--- nvim/.config/nvim/lua/plugins/mini.ai.lua
+++ nvim/.config/nvim/lua/plugins/mini.ai.lua
@@ -14,6 +14,7 @@ return {
       custom_textobjects = {
         f = gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }),
         c = gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }),
+        s = gen_spec.treesitter({ a = "@statement.outer", i = "@assignment.rhs" }),
         o = gen_spec.treesitter({
           a = { "@block.outer", "@conditional.outer", "@loop.outer" },
           i = { "@block.inner", "@conditional.inner", "@loop.inner" },
