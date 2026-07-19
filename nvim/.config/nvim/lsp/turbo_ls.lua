9e932e003525f9938ef305789a9ae12302f0ea9c
temarych <teemest06@icloud.com>
2026-07-19 12:29:19 +0300

    revert: feat(turbo): add turbo ls
    This reverts commit 817ab63c0f041823f5994e6d22be485f8e36202c.

--- nvim/.config/nvim/lsp/turbo_ls.lua
+++
@@ -1,6 +0,0 @@
----@type vim.lsp.Config
-return {
-  cmd = { "turbo-language-server", "--stdio" },
-  filetypes = { "html", "ruby", "eruby", "blade", "php" },
-  root_markers = { "Gemfile", ".git" },
-}
